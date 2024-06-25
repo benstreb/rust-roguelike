4784133b5e5468417942280e969fb72b2fba2907
Benjamin Streb <bstreb@gmail.com>
2024-06-25 16:27:26 -0700

    Bracket-lib smoke test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use rusqlite::{Connection, Result};
+use bracket_lib::prelude::{main_loop, BResult, BTerm, BTermBuilder, GameState};
+use rusqlite::Connection;
 
-#[derive(Debug)]
-struct Person {
-    id: i32,
-    name: String,
-    data: Option<Vec<u8>>,
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World");
+    }
 }
 
-fn main() -> Result<()> {
+fn main() -> BResult<()> {
     let conn = Connection::open_in_memory()?;
 
     conn.execute(
@@ -18,27 +20,10 @@ fn main() -> Result<()> {
         )",
         (), // empty list of parameters.
     )?;
-    let me = Person {
-        id: 0,
-        name: "Steven".to_string(),
-        data: None,
-    };
-    conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        (&me.name, &me.data),
-    )?;
 
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-    let person_iter = stmt.query_map([], |row| {
-        Ok(Person {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            data: row.get(2)?,
-        })
-    })?;
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
-    }
-    Ok(())
+    let context = BTermBuilder::simple80x50()
+        .with_title("Hello Rust World")
+        .build()?;
+    let gs = State {};
+    main_loop(context, gs)
 }
